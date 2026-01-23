e6b22b9b6c75c6e81a0223b72ff8adf921831f0d
Mario Taddeucci <mariotaddeucci@gmx.com>
2026-01-23 07:03:15 -0300

    Bump version to 0.2.0 in Cargo.toml, pyproject.toml, and __init__.py

--- src/charsetrs/__init__.py
+++ src/charsetrs/__init__.py
@@ -13,10 +13,7 @@ from charsetrs._internal import (
     normalize_file_stream as _normalize_file_stream_internal,
 )
 
-try:
-    from charsetrs.version import __version__  # type: ignore
-except ImportError:
-    __version__ = "0.0.0+unknown"
+__version__ = "0.2.0"
 
 __all__ = [
     "analyse",
